c541a66d5d8ae29aea2ed1bd738838eeddf15988
Jelmer <jelmer339@gmail.com>
2021-08-21 11:27:04 +0200

    DatabasePrimer + StockTest
    and other setup files

--- tests/StockTest.php
+++ tests/StockTest.php
@@ -2,7 +2,23 @@
 
 namespace App\Tests;
 
-class StockTest
-{
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
+class StockTest extends KernelTestCase
+{
+    private $entityManager;
+
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+
+        DatabasePrimer::prime($kernel);
+        $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+    }
+
+    public function testItWorks()
+    {
+        $this->assertTrue(true);
+    }
 }
